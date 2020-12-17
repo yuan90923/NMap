f633320d7d3f9fc6fd9bf9cf72f3820e5711fb08
Neil <yuan90923@gmail.com>
2020-12-17 17:56:02 +0800

    add address route and controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::group(['prefix' => 'Map', 'namespace' => 'App\Http\Controllers\Map'], function() {
+    Route::get('/', 'AddressController@getAddress');
 });
