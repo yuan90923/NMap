a4f0eacfc281df550e35f0a34c02783ed3e8c99c
Neil <yuan90923@gmail.com>
2020-12-17 17:56:35 +0800

    補推controller

---
+++ app/Http/Controllers/Map/AddressController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Map;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AddressController extends Controller
+{
+    // 地址查詢
+    public function getAddress(Request $request)
+    {
+        //
+    }
+}
